bb5e7dbf90b508b8af8e42463fb09adb31800d53
ruchi pandey <pandeyruchi.30@gmail.com>
2020-11-22 16:42:36 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    aws_table_name: 'dynamodb-test',
+    aws_local_config: {
+        //Provide details for local configuration
+    },
+    aws_remote_config: {
+        accessKeyId: '',
+        secretAccessKey: '',
+        region: 'us-east-1',
+    }
+};
